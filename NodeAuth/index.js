2bde0f06d802e8804cdca10868bf4a2eded80a48
qorlgns1 <rlgns0610@gmail.com>
2022-12-02 22:14:42 +0900

    chore: pg 설치 후 연결

--- NodeAuth/index.js
+++ NodeAuth/index.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { Client } = require("pg");
+const Query = require("pg").Query;
+
+const dotenvConfig = require("dotenv").config().parsed;
+
+const client = new Client({
+  user: dotenvConfig.user,
+  host: dotenvConfig.host,
+  database: dotenvConfig.database,
+  password: dotenvConfig.password,
+  port: dotenvConfig.port,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("success!");
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
